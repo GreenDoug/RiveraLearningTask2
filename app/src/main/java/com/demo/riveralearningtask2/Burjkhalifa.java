f6f2629037836ce91a648c9ddcd4f5b9aee7ef1e
Matthew <matthewty94@gmail.com>
2022-10-28 21:02:28 +0800

    Initial commit

---
+++ app/src/main/java/com/demo/riveralearningtask2/Burjkhalifa.java
@@ -0,0 +1,14 @@
+package com.demo.riveralearningtask2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Burjkhalifa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_burjkhalifa);
+    }
+}
